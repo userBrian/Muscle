36eccbbe20cfb74d427915c2e41d1c4e746818f0
neofoetus <corentin.manscour@u-psud.fr>
2017-12-10 19:24:18 +0100

    Regulateurs bientot fonctionnels

---
+++ src/Vehicule.java
@@ -0,0 +1,57 @@
+import java.util.Observable;
+
+
+public abstract class Vehicule extends Observable{
+
+	/**
+	 * @param args
+	 */
+	
+	private final String id;
+	protected int vitesse;
+	private int position;
+	private Segment dest;
+	
+	//manque l'initialisation du segment
+	public Vehicule(String id) {
+		this.id = id;
+		this.vitesse = 0;
+		this.position = 0;
+	}
+	
+	
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+		setChanged();
+		notifyObservers();
+	}
+
+	public int getVitesse() {
+		return vitesse;
+	}
+
+	public abstract void setVitesse(int vitesse);
+	
+	public Segment getDest() {
+		return dest;
+	}
+
+	public void setDest(Segment dest) {
+		this.dest = dest;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
